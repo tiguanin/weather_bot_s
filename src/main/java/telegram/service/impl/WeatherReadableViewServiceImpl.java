17ddab290612e41582a6ee633f72df140ddbf9cf
a_lutsenko <a_lutsenko@nposinergy.ru>
2018-09-30 11:11:13 +0300

    debugging features (jenkins) 2

--- src/main/java/telegram/service/impl/WeatherReadableViewServiceImpl.java
+++ src/main/java/telegram/service/impl/WeatherReadableViewServiceImpl.java
@@ -11,7 +11,7 @@ public class WeatherReadableViewServiceImpl implements WeatherReadableViewServic
     public String getConvertedForecastAsString(FactualWeather factualWeather) {
         String data = "Доброе утро, Александр! \uD83D\uDE0E \n" +
                 "На данный момент температура на улице: " +
-                (factualWeather.getTemp() >= 10 ? "терпимо" : "ХОЛОДНО! \uD83C\uDF85\uD83C\uDFFB, ") +
+                (factualWeather.getTemp() >= 10 ? "терпимо" : "ОЧХОЛОДНО! \uD83C\uDF85\uD83C\uDFFB, ") +
                 "а именно " + factualWeather.getTemp() + " градусов,\n " +
                 "ощущается как " + factualWeather.getFeelsLike() +
                 (factualWeather.getFeelsLike() >= 3 ? ", можно надеть шарф \uD83E\uDDE3" : ", надевай теплую куртку! \uD83D\uDC36") ;
